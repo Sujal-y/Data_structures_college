ab39575200cdda85b515761d663cd22b6cdd0b34
Sujal-y <sujalyasho2006@gmail.com>
2025-08-23 12:38:58 +0530

    Update lab 3

---
+++ Lab_programs/Lab3/string_operations.c
@@ -0,0 +1,22 @@
+// Write a program to perform following string operations without using string
+// handling functions:
+// a.) length of the string
+// b.) string concatenation
+// c.) string comparison
+// d.) to insert a sub string
+// e.) to delete a substring
+#include<stdio.h>
+#include <stdlib.h>
+
+int length_of_string() {
+return 0;
+}
+char* string_concatenation() {
+    char * str = "hello";
+    return str;
+}
+int string_compare() {
+    int istrue;
+    return istrue;
+}
+
